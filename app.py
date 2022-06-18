7f087ede59004e56c99f5c92e94c8006d208e4bd
UT <tewariutkarsh@outlook.com>
2022-06-18 16:00:16 +0530

    Dockerfile added

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ def index():
 port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
     host = '0.0.0.0'
-    # port = 5000
+    
     httpd = simple_server.make_server(host, port, app)
-    print("Serving on http://127.0.0.1:5000/")
+    # print("Serving on http://127.0.0.1:5000/")
     # print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
 
